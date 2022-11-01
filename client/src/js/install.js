057bc6f3ddb15ca9d0645a6c8c89bbd940d31c3e
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-11-01 16:03:10 -0600

    got the intall button to hide if the app is installed

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -16,24 +16,11 @@ butInstall.addEventListener('click', async () => {
   if (!promptEvent) {
     return;
   }
-
   // Show prompt
   promptEvent.prompt();
-
   // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
-
   butInstall.classList.toggle('hidden', true);
-
-  // if (window.deferredPrompt !== null) {
-  //   window.deferredPrompt.prompt();
-  //   const { answer } = await window.deferredPrompt.userChoice;
-  //   if (outcome === 'accepted') {
-  //     window.deferredPrompt = null;
-  //     butInstall.classList.toggle('hidden', true);
-  //   }
-  //   console.log(`${answer}`);
-  // }
 });
 
 // TODO: Add an handler for the `appinstalled` event DONE
