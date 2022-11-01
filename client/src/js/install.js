8816e0dfc9d2dd451f023a4d9a8eaa0a645ce822
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-11-01 15:34:58 -0600

    install works

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -11,15 +11,29 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element DONE
 butInstall.addEventListener('click', async () => {
-  if (window.deferredPrompt !== null) {
-    window.deferredPrompt.prompt();
-    const { answer } = await window.deferredPrompt.userChoice;
-    if (outcome === 'accepted') {
-      window.deferredPrompt = null;
-      butInstall.classList.toggle('hidden', true);
-    }
-    console.log(`${answer}`);
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
   }
+
+  // Show prompt
+  promptEvent.prompt();
+
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle('hidden', true);
+
+  // if (window.deferredPrompt !== null) {
+  //   window.deferredPrompt.prompt();
+  //   const { answer } = await window.deferredPrompt.userChoice;
+  //   if (outcome === 'accepted') {
+  //     window.deferredPrompt = null;
+  //     butInstall.classList.toggle('hidden', true);
+  //   }
+  //   console.log(`${answer}`);
+  // }
 });
 
 // TODO: Add an handler for the `appinstalled` event DONE
